22883e9bf12faa1327991b4af102e9e5b6d679bc
eareyan <enrique3@gmail.com>
2021-07-11 19:46:46 -0400

    plots with bounds

--- plots.py
+++ plots.py
@@ -1,35 +1,104 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import pandas as pd
+
+from algorithms import compute_schedule_length
+from stats import compute_sample_complexity, compute_simulation_complexity
+
+
+def bounds_line(num_discard_cards):
+
+    # TODO: Parameters of experiments: this is probably bad practice and needs to be refactored.
+    exp_target_delta = 0.05
+    exp_target_eps = 0.01
+    beta = 1.1
+
+    schedule_length = compute_schedule_length(target_epsilon=exp_target_eps, beta=beta)
+    size_of_game = 100 if num_discard_cards == 2 else 25
+    v_inf_resolution_grid = [i * 0.01 for i in range(0, 100)]
+    v_1_inf_resolution_grid = [i for i in range(0, size_of_game)]
+
+    v_inf_grid = [
+        compute_sample_complexity(
+            schedule_length=schedule_length,
+            epsilon=exp_target_eps,
+            delta=exp_target_delta,
+            v_inf=x,
+            game={"size_of_game": size_of_game},
+            c=2,
+            beta=beta,
+        )
+        for x in [i * 0.01 for i in range(0, 100)]
+    ]
+
+    v_1_inf_grid = [
+        compute_simulation_complexity(
+            schedule_length=schedule_length,
+            epsilon=exp_target_eps,
+            delta=exp_target_delta,
+            v_1_inf=x,
+            game={"size_of_game": size_of_game},
+            c=2,
+            beta=beta,
+        )
+        for x in v_1_inf_resolution_grid
+    ]
+
+    return {
+        "v_inf_grid": {"x": v_inf_resolution_grid, "y": v_inf_grid},
+        "v_1_inf_grid": {"x": v_1_inf_resolution_grid, "y": v_1_inf_grid},
+    }
 
 
 def game_scatter_plot(results, num_discard_cards):
+    # Select results based on num_discard_cards
     results_to_plot = results[results["num_discard_cards"] == num_discard_cards]
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    fig.suptitle(f"Results num_discard_cards = {num_discard_cards}")
 
+    # Plotting side-by-side.
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+
+    # Set plot title.
+    if num_discard_cards == 1:
+        fig.suptitle(f"Players discard {num_discard_cards} card.")
+    else:
+        fig.suptitle(f"Players discard {num_discard_cards} cards.")
+
+    # Scatter plot for Empirical sample complexity
     x = results_to_plot["v_inf"]
     y = results_to_plot["emp_sample_complexity"]
-    print(x.min(), x.max())
-    ax1.plot(x, y, "o", color="black")
-    ax1.set_title("emp_sample_complexity")
+    ax1.plot(x, y, "o", color="blue")
+    ax1.set_title("Empirical sample complexity")
     ax1.set_xlim(0, 1)
-    ax1.set_xlabel("v_inf")
+    ax1.set_ylim(0)
+    ax1.yaxis.set_major_formatter(mtick.FormatStrFormatter("%.1e"))
+    ax1.set_xlabel(r"$||v||_{\infty}$")
 
+    # Scatter plot for Empirical simulation complexity
     x = results_to_plot["v_1_inf"]
     y = results_to_plot["emp_simulation_complexity"]
     ax2.plot(x, y, "o", color="red")
-    ax2.set_title("emp_simulation_complexity")
-    # ax2.set_xlim(0, 1)
-    ax2.set_xlabel("v_1_inf")
+    ax2.set_title("Empirical simulation complexity")
+    ax2.set_xlim(0, 100 if num_discard_cards == 2 else 25)
+    ax2.set_ylim(0)
+    ax2.yaxis.set_major_formatter(mtick.FormatStrFormatter("%.1e"))
+    ax2.set_xlabel(r"$||v||_{1,\infty}$")
 
-    # plt.show()
+    # Plot bounds
+    bounds = bounds_line(num_discard_cards=num_discard_cards)
+    ax1.plot(bounds["v_inf_grid"]["x"], bounds["v_inf_grid"]["y"], "--", color="black")
+    ax2.plot(
+        bounds["v_1_inf_grid"]["x"], bounds["v_1_inf_grid"]["y"], "--", color="black"
+    )
+
+    # Plot and save figure.
     figure = plt.gcf()
     figure.set_size_inches(12, 6)
     plt.savefig(f"plots/num_discard_cards-{num_discard_cards}.png", bbox_inches="tight")
 
 
 if __name__ == "__main__":
+
+    # Read results
     games = pd.read_csv("results/games.csv")
     psp_runs = pd.read_csv("results/psp_runs.csv")
 
@@ -41,5 +110,6 @@ if __name__ == "__main__":
     )
     exp_results.to_csv("results/join.csv", index=None)
 
+    # Plot results.
     game_scatter_plot(results=exp_results, num_discard_cards=1)
     game_scatter_plot(results=exp_results, num_discard_cards=2)
